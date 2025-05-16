d517cb129f2f30fe9910eb9b2bfe06425a4c0df3
Chayun Fadila <128894491+chayunfdl@users.noreply.github.com>
2025-05-17 05:09:43 +0700

    update db

--- src/models/db.js
+++ src/models/db.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',            // ganti jika bukan 'root'
-  password: '',
-  database: 'worldbank_db',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
